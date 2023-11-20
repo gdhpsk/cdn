187638c4d56de2e8fbf6c790ca1b02ca21674e9d
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-11-20 15:14:01 -0800

    still need wildcard for cors

--- src/pages/api/bucket/[[...path]].ts
+++ src/pages/api/bucket/[[...path]].ts
@@ -20,6 +20,7 @@ function escapeRegExp(string: string) {
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
     let user = ""
     let specifiedPath = ""
     try {
