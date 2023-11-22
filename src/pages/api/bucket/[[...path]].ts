5078b5054f7d97e66387658b4d68ea3b09568d79
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-11-22 01:13:52 -0800

    didnt work :(

--- src/pages/api/bucket/[[...path]].ts
+++ src/pages/api/bucket/[[...path]].ts
@@ -15,49 +15,6 @@ let { bucket } = process.env
 function escapeRegExp(string: string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
-
-  function escapeSpecialCharacters(fileName: string) {
-    // Define a mapping of special characters and their escaped representations
-    const specialCharacterMap: Record<any, any> = {
-        '!': '\\!',
-        '@': '\\@',
-        '#': '\\#',
-        '$': '\\$',
-        '%': '\\%',
-        '^': '\\^',
-        '&': '\\&',
-        '*': '\\*',
-        '(': '\\(',
-        ')': '\\)',
-        '-': '\\-',
-        '_': '\\_',
-        '+': '\\+',
-        '=': '\\=',
-        '{': '\\{',
-        '}': '\\}',
-        '[': '\\[',
-        ']': '\\]',
-        '|': '\\|',
-        ';': '\\;',
-        ':': '\\:',
-        '\'': '\'',
-        '"': '\\"',
-        '<': '\\<',
-        '>': '\\>',
-        ',': '\\,',
-        '.': '\\.',
-        '/': '/',
-        '?': '\\?',
-        '`': '\\`',
-        '~': '\\~',
-        '\\': '\\\\'
-    };
-
-    // Replace each special character with its escaped representation
-    const escapedFileName = fileName.replace(/[!@#$%^&*()\-_+={}\[\]|;:'"<>.,\/?`~\\]/g, match => specialCharacterMap[match as any] || match);
-
-    return escapedFileName;
-}
   
 
 
@@ -302,7 +259,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse,
                         if(!req.query.overwriteGroup && exists !== null && exists?.directory == existing.directory) return res.status(403).send({error: "403 FORBIDDEN", message: "That new object directory already exists. Pass the overwriteGroup param to try and overwrite it.", type: "GroupOverwriteErr"})
                         if(exists !== null  && exists?.directory !== existing.directory) return res.status(403).send({error: "403 FORBIDDEN", message: `That new object directory is of type "${exists.directory ? "Folder" : "File"}", meaning you cannot overwrite it!`})
                     if(req.query.overwriteGroup) {await mappings.deleteOne({path: req.body.newDir})};
-                    await fs.rename(bucket as string + "/" + escapeSpecialCharacters((req.query.path as string[]).join("/")), bucket as string + escapeSpecialCharacters(req.body.newDir))
+                    await fs.rename(bucket as string + "/" + (req.query.path as string[]).join("/"), bucket as string + req.body.newDir)
                     await mappings.updateMany({path: new RegExp("^/" + escapeRegExp((req.query.path as string[]).join("/")) + "($|/)")}, [{
                         $set: {
                             path: {
