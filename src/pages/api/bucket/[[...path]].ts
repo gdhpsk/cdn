3ff8843eb188d82129e02c4035cdb6fe3edab01a
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-11-26 23:00:02 -0800

    you can turn off / on partial content manually

--- src/pages/api/bucket/[[...path]].ts
+++ src/pages/api/bucket/[[...path]].ts
@@ -318,6 +318,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse,
                     
                     if(stat.isDirectory()) throw new Error()
                     function iOS() {
+                        if(req.query.partialContent == "false") return true;
+                        if(req.query.partialContent == "true") return false;
                         return [
                           'iPad',
                           'iPhone',
